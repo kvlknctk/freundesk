4ec7e3f93c636dd66d348d660f89254e560dabb7
kvlknctk <AuxDell76?>
2020-10-31 00:55:07 +0300

    clear

--- app/components/LeftController.tsx
+++ app/components/LeftController.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {useDispatch, useSelector} from 'react-redux';
-import {Switch} from 'react-native-paper';
-
-import * as themeActions from 'app/store/actions/themeActions';
+import { useSelector} from 'react-redux';
 import {IThemeState} from 'app/models/reducers/theme';
 import NavigationService from "../navigation/NavigationService";
 
@@ -12,23 +9,13 @@ interface IState {
     themeReducer: IThemeState;
 }
 
-interface IStateBasket {
-    basketReducer: any;
-}
-
 const LeftController: React.FC = () => {
     const isDark = useSelector((state: IState) => state.themeReducer.isDark);
-    const basketStore = useSelector((state: IStateBasket) => state.basketReducer);
     const goBasket = () => NavigationService.navigate('Home');
-
-    const dispatch = useDispatch();
-    const onToggleTheme = () => dispatch(themeActions.setIsDarkTheme(!isDark));
-    //const iconName = isDark ? 'weather-night' : 'white-balance-sunny';
     const iconColor = isDark ? 'white' : 'black';
 
     return (
         <View style={styles.container}>
-
             <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}} onPress={goBasket}>
                 <Icon name="home" size={25} style={styles.icon} color={iconColor}/>
             </TouchableOpacity>
