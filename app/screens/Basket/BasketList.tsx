99ff03537b434b2bc9d498399eb1a8bd9e179196
kvlknctk <AuxDell76?>
2020-11-01 15:07:08 +0300

    basket customized for no added product.

--- app/screens/Basket/BasketList.tsx
+++ app/screens/Basket/BasketList.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import * as basketActions from "../../store/actions/basketActions";
 import {useDispatch, useSelector} from "react-redux";
+import {basketDrop} from "../../store/actions/basketActions";
 
 interface IStateBasket {
     basketReducer: any;
@@ -16,8 +17,16 @@ const BasketList: React.FC = () => {
     const dropBasket = () => dispatch(basketActions.basketDrop());
     const basketStore = useSelector((state: IStateBasket) => state.basketReducer);
 
-    const reducerf = (accumulator, currentValue) => accumulator + parseFloat(currentValue.price);
+    const reducerf = (accumulator: any, currentValue: any) => accumulator + parseFloat(currentValue.price);
+    // @ts-ignore
     const sumBasket = (addedProducts: object) => addedProducts.reduce(reducerf, 0);
+
+    if  (basketStore.addedProducts.length === 0){
+        return <View style={{alignItems: 'center', justifyContent: "center", flex: 1}}>
+            <Text style={{color: 'white', fontSize: 20}}>You didn't add product.</Text>
+
+        </View>
+    }
 
     return (
         <View style={{flexDirection: 'column', justifyContent: 'space-between', flex: 1}}>
@@ -50,40 +59,44 @@ const BasketList: React.FC = () => {
 
             </ScrollView>
 
-            <View>
-                <View style={{ flexDirection: 'column'}}>
-                    <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 20}}>
-                        <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>Total</Text>
-                        <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>
-                            {
-                                sumBasket(basketStore.addedProducts).toFixed(2)
-                            } £
-                        </Text>
-                    </View>
-
-                    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                        <Text style={{color: 'white', fontSize: 30}}>Estimated</Text>
-                        <Text style={{color: 'white', fontSize: 30}}>
-                            21 Min
-                        </Text>
-                    </View>
-
-                    <TouchableOpacity
-                        style={{justifyContent: 'center'}}
-                        onPress={() => console.log("asd")}>
-                        <View style={{height: 50, backgroundColor: 'aquamarine'}}>
-                            <Text style={{color: 'black', textAlign: 'center', fontSize: 30}}>Order Now</Text>
+            {
+                basketStore.addedProducts.length > 0 &&
+                <View>
+                    <View style={{ flexDirection: 'column'}}>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 20}}>
+                            <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>Total</Text>
+                            <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>
+                                {
+                                    sumBasket(basketStore.addedProducts).toFixed(2)
+                                } £
+                            </Text>
                         </View>
-                    </TouchableOpacity>
 
-                </View>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <Text style={{color: 'white', fontSize: 30}}>Estimated</Text>
+                            <Text style={{color: 'white', fontSize: 30}}>
+                                21 Min
+                            </Text>
+                        </View>
 
-                {/* <TouchableOpacity onPress={() => dropBasket()}>
+                        <TouchableOpacity
+                            style={{justifyContent: 'center'}}
+                            onPress={() => dropBasket()}>
+                            <View style={{height: 50, backgroundColor: 'aquamarine'}}>
+                                <Text style={{color: 'black', textAlign: 'center', fontSize: 30}}>Order Now</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                    </View>
+
+                    {/* <TouchableOpacity onPress={() => dropBasket()}>
                     <View style={{padding: 5}}>
                         <Text style={{color: 'white', fontSize: 25}}>Clear Baskets</Text>
                     </View>
                 </TouchableOpacity>*/}
-            </View>
+                </View>
+
+            }
 
         </View>
     );
